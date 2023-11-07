0a77d5413b577fda8d5b940d5e4aa442c190dcfc
mehedi hasan <mdmehedihasananik111@gmail.com>
2023-11-07 23:58:06 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
 
-app.use(cors())
+                app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
